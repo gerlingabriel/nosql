7a6124d34d6417a7cf15e6ff32fa1f32d2311e7a
Gabriel Gerlin <gerlin.gabriel@gmail.com>
2021-08-22 18:42:57 -0300

    first commit

---
+++ src/main/java/com/project/nosql/NosqlApplication.java
@@ -0,0 +1,13 @@
+package com.project.nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nosql/NosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
